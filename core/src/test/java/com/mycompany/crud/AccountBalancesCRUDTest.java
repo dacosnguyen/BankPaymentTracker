8a85effcd547e5341e05403a365bc3fb3c7f9db1
Dacos Nguyen <dacos.nguyen@gmail.com>
2020-01-14 04:02:35 +0100

    Parallel update in AccountBalancesCRUDTest enhanced.

--- core/src/test/java/com/mycompany/crud/AccountBalancesCRUDTest.java
+++ core/src/test/java/com/mycompany/crud/AccountBalancesCRUDTest.java
@@ -1,12 +1,14 @@
 package com.mycompany.crud;
 
 import com.mycompany.persistence.Account;
+import com.mycompany.types.AccountBalance;
 import com.mycompany.types.Currency;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.math.BigDecimal;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -43,17 +45,27 @@ public class AccountBalancesCRUDTest {
         crud.update("EUR -250");
         crud.update("USD -150");
         crud.update("EUR 350");
-        parallelUpdate();
+        updateAccountBalancesInParallel(() ->
+                crud.update("CZK 100")
+        );
+        updateAccountBalancesInParallel(() -> {
+            crud.update("GBP 10");
+            crud.update("VND 10");
+        });
 
-        Assert.assertEquals(Account.INSTANCE.getCurrencyAccountBalanceMap().get(Currency.USD).getAmount(), new BigDecimal(-50));
-        Assert.assertEquals(Account.INSTANCE.getCurrencyAccountBalanceMap().get(Currency.EUR).getAmount(), new BigDecimal(100));
-        Assert.assertEquals(Account.INSTANCE.getCurrencyAccountBalanceMap().get(Currency.CZK).getAmount(), new BigDecimal(100000));
+        Map<Currency, AccountBalance> currencyAccountBalanceMap = Account.INSTANCE.getCurrencyAccountBalanceMap();
+        Assert.assertEquals(currencyAccountBalanceMap.get(Currency.USD).getAmount(), new BigDecimal(-50));
+        Assert.assertEquals(currencyAccountBalanceMap.get(Currency.EUR).getAmount(), new BigDecimal(100));
+        Assert.assertEquals(currencyAccountBalanceMap.get(Currency.CZK).getAmount(), new BigDecimal(100000));
+        Assert.assertEquals(currencyAccountBalanceMap.get(Currency.GBP).getAmount(), new BigDecimal(10000));
+        Assert.assertEquals(currencyAccountBalanceMap.get(Currency.VND).getAmount(), new BigDecimal(10000));
     }
 
     /**
-     * Updates one account balance in parallel with 500 threads.
+     * Updates 1000 times account balances in parallel with 500 threads.
+     * @param updateCommands crud.update commands with bank transfers.
      */
-    private void parallelUpdate() throws InterruptedException {
+    private void updateAccountBalancesInParallel(Runnable updateCommands) throws InterruptedException {
         ExecutorService executorService = Executors.newFixedThreadPool(500);
         for (int i = 0; i < 1000; i++) {
             executorService.execute(() -> {
@@ -62,7 +74,7 @@ public class AccountBalancesCRUDTest {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                crud.update("CZK 100");
+                updateCommands.run();
             });
         }
         executorService.shutdown();
