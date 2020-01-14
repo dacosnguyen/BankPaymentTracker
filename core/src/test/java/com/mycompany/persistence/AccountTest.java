a9bee1c758c3b94b567edd568bdc6d186c1ac7a7
Dacos Nguyen <dacos.nguyen@gmail.com>
2020-01-14 02:46:29 +0100

    Added a unit test AccountTest.

---
+++ core/src/test/java/com/mycompany/persistence/AccountTest.java
@@ -0,0 +1,34 @@
+package com.mycompany.persistence;
+
+import com.mycompany.types.AccountBalance;
+import com.mycompany.types.Currency;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+import static org.testng.Assert.assertEquals;
+
+public class AccountTest {
+
+    private Map<Currency, AccountBalance> currencyAccountBalanceMap;
+
+    @BeforeClass
+    public void init() {
+        currencyAccountBalanceMap = Account.INSTANCE.getCurrencyAccountBalanceMap();
+    }
+
+    @Test
+    public void testNumberOfAccountBalancesEqualsToSupportedCurrencies() {
+        assertEquals(currencyAccountBalanceMap.size(), Currency.values().length);
+    }
+
+    @Test
+    public void testBalancesEqualsToZero() {
+        currencyAccountBalanceMap.values().forEach(
+                accountBalance -> Assert.assertEquals(BigDecimal.ZERO.compareTo(currencyAccountBalanceMap.get(accountBalance.getCurrency()).getAmount()), 0)
+        );
+    }
+}
